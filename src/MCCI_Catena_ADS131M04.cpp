551346a25df2f86c53fb3a4465b2c4194f0a3bab
Pranau-R <pranau@mcci.com>
2022-10-11 12:07:33 +0530

    WIP: Initial Commit

---
+++ src/MCCI_Catena_ADS131M04.cpp
@@ -0,0 +1,18 @@
+/*
+
+Module: MCCI_CATENA_ADS131M04.cpp
+
+Function:
+    Implementation code for MCCI Catena ADS131M04 Four-channel, 24-bit, 64-kSPS, simultaneous-sampling, delta-sigma ADC library.
+
+Copyright and License:
+    See accompanying LICENSE file for copyright and license information.
+
+Author:
+    Pranau R, MCCI Corporation   October 2022
+
+*/
+
+#include "MCCI_Catena_ADS131M04.h"
+
+/**** end of MCCI_Catena_ADS131M04.cpp ****/
---
+++ src/MCCI_Catena_ADS131M04.h
@@ -0,0 +1,23 @@
+/*
+
+Module: MCCI_Catena_ADS131M04.h
+
+Function:
+    Top-level include file for MCCI Catena ADS131M04 Four-channel, 24-bit, 64-kSPS, simultaneous-sampling, delta-sigma ADC library.
+
+Copyright and License:
+    See accompanying LICENSE file for copyright and license information.
+
+Author:
+    Pranau R, MCCI Corporation   October 2022
+
+*/
+
+/// \file
+
+#ifndef _MCCI_CATENA_ADS131M04_H_
+#define _MCCI_CATENA_ADS131M04_H_ /* prevent multiple includes */
+
+#pragma once
+
+#endif /* _MCCI_CATENA_ADS131M04_H_ */
